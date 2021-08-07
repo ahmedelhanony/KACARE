817fbfcb058fec56cc8851476692302791a2c20b
ahmed elhanony <aelhanony.c@stc.com.sa>
2021-08-07 14:11:27 +0300

    restructur layout

--- src/app/Shared/layout/layout.component.ts
+++ src/app/Shared/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
-
+  @Input() isPortal = true;
   constructor() { }
 
   ngOnInit(): void {
